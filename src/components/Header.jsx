ae6dad657d8857e472c55abec0181251063df9f4
Muhammad Shahbaz <M023Shah@gmail.com>
2022-01-15 13:53:44 -0800

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { ReactComponent as CloseMenu } from "../assets/x.svg";
+import { ReactComponent as MenuIcon } from "../assets/menu.svg";
+import { ReactComponent as Logo } from "../assets/cube.svg";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      
+    </div>
+  );
+};
+
+export default Header;
